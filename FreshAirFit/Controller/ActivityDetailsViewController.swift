f6fc97d3f61bbd0bc72e02af1ee045db58d75d45
Banana Viking <bananavikingcoder@gmail.com>
2018-10-08 16:07:24 -0500

    add cancel and done button to ActivityDetailsVC

---
+++ FreshAirFit/Controller/ActivityDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityDetailsViewController.swift
+//  FreshAirFit
+//
+//  Created by Banana Viking on 10/8/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+import UIKit
+
+class ActivityDetailsViewController: UITableViewController {
+    
+    @IBAction func done() {
+        
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+}
