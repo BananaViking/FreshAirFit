534d70581dd0965dd6e005575fa560fe86b00633
Banana Viking <bananavikingcoder@gmail.com>
2018-10-08 14:24:23 -0500

    first commit

---
+++ FreshAirFit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreshAirFit
+//
+//  Created by Banana Viking on 10/8/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
