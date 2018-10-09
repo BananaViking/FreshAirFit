0b1dce062420020cb0ee7dfce6c5eb54f6382cef
Banana Viking <bananavikingcoder@gmail.com>
2018-10-08 20:38:44 -0500

    added UserDefaults to Activity.swift

--- FreshAirFit/Model/Activity.swift
+++ FreshAirFit/Model/Activity.swift
@@ -8,23 +8,50 @@
 
 import UserNotifications
 
-class Activity {
-    var description = "Add a description for your activity..."
+class Activity: NSObject, Codable {
     var lowTemp = ""
     var highTemp = ""
     var shouldNotify = false
     var notifyTime = Date()
-    var notifyText = ""
+    var notifyText = ""  // need to make this something like "tomorrow will be 95. might be a nice day for a swim" and add weather info
+    var activityID: Int
+    
+    override init() {
+        activityID = Activity.nextActivityID()
+        super.init()
+    }
+    
+    class func nextActivityID() -> Int {
+        let userDefaults = UserDefaults.standard
+        let activityID = userDefaults.integer(forKey: "ActivityID")
+        userDefaults.set(activityID + 1, forKey: "ActivityID")
+        userDefaults.synchronize()
+        return activityID
+    }
     
     func scheduleNotification() {  // HQ Daily.swift
         removeNotification()
         if shouldNotify {
             let content = UNMutableNotificationContent()
             content.title = "Just thought you'd like to know..."
+            content.body = notifyText
+            content.sound = UNNotificationSound.default
+            
+            let calendar = Calendar(identifier: .gregorian)
+            let components = calendar.dateComponents([.hour, .minute], from: notifyTime)
+            let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+            let request = UNNotificationRequest(identifier: "\(activityID)", content: content, trigger: trigger)
+            let center = UNUserNotificationCenter.current()
+            center.add(request)
         }
     }
     
     func removeNotification() {  // HQ Daily.swift
-        
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: ["\(activityID)"])
+    }
+    
+    deinit {
+        removeNotification()
     }
 }
