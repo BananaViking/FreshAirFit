05ac85ed0f87aae35549a5c69f0829fc9825fd1f
Banana Viking <bananavikingcoder@gmail.com>
2018-10-10 19:36:09 -0500

    added WeatherCondition.swift to Model

---
+++ FreshAirFit/Model/WeatherCondition.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCondition.swift
+//  FreshAirFit
+//
+//  Created by Banana Viking on 10/10/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCondition {
+    var weatherIcon: UIImage?
+    var weatherConditionDescription = ""
+    var isChecked = false
+}
