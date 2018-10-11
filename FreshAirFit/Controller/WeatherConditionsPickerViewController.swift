987052a24c48fd354733cdd79f550f3099ad1f80
Banana Viking <bananavikingcoder@gmail.com>
2018-10-10 19:22:42 -0500

    added new row and VC for weatherConditions

---
+++ FreshAirFit/Controller/WeatherConditionsPickerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherConditionsPicker.swift
+//  FreshAirFit
+//
+//  Created by Banana Viking on 10/10/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+import UIKit
+
+class WeatherConditionsTableViewCell: UITableViewCell {
+    // setup weatherIcon and weatherDescription outlets here
+}
+
+class WeatherConditionsPickerViewController: UITableViewController {
+    
+}
