f8882b41e6172ddbe5d56ba224ebdbcf291eef86
Banana Viking <bananavikingcoder@gmail.com>
2018-10-10 19:31:58 -0500

    set up weatherIcon and weatherCondition for WCPVC

--- FreshAirFit/Controller/WeatherConditionsPickerViewController.swift
+++ FreshAirFit/Controller/WeatherConditionsPickerViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class WeatherConditionsTableViewCell: UITableViewCell {
-    // setup weatherIcon and weatherDescription outlets here
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var weatherCondition: UILabel!
 }
 
 class WeatherConditionsPickerViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell") as! WeatherConditionsTableViewCell
+//        cell.activityDescriptionLabel?.text = activities[indexPath.row].activityDescription
+        cell.weatherIcon.image = UIImage(named: "sunny")
+        cell.weatherCondition.text = "Sunny"
+        return cell
+    }
+    
+    // need to add deselect row after click, add checkmark, feed data back to other VC's
+    // make an array for all checked weather conditions and AVC label should display "Weather: " then list whole array for checked conditions
 }
